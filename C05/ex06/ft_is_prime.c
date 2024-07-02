df13ffdec77695caa3a63b90949c53be2a48154f
Mathieu Taramarcaz <mtaramar@c2r9s5.42lausanne.ch>
2024-07-02 17:41:43 +0200

    C05

--- C05/ex06/ft_is_prime.c
+++ C05/ex06/ft_is_prime.c
@@ -6,11 +6,32 @@
 /*   By: mtaramar <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 16:13:30 by mtaramar          #+#    #+#             */
-/*   Updated: 2024/07/02 16:15:40 by mtaramar         ###   ########.fr       */
+/*   Updated: 2024/07/02 17:36:57 by mtaramar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int	ft_is_prime(int nb)
 {
-	
+	int	i;
+
+	i = 2;
+	if (nb < 2)
+		return (0);
+	if (nb == 2)
+		return (1);
+	while (i < nb)
+	{
+		if ((nb % i) == 0)
+			return (0);
+		i++;
+	}
+	return (1);
 }
+/*
+int main(void)
+{
+	printf("%d", ft_is_prime(12));
+	return (0);
+}*/
